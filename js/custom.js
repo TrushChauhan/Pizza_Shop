2fa120feefd5916bcc62a29648d14b73d4f5c542
TrushChauhan <tatva.pci203@outlook.com>
2025-01-30 18:36:53 +0530

    make a user edit page

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,16 @@ function addUser(){
 function backToUsers(){
     window.location.href='users.html';
 }
+function editUser(){
+    window.location.href='edit-user.html'
+}
+function deleteUserModal(){
+    const ele=document.getElementById("modal-del");
+    ele.style.display="flex";
+    document.getElementById("entire-frame").style.opacity=0.7;
+}
+function removeModal(){
+    const ele=document.getElementById("modal-del");
+    ele.style.display="none";
+    document.getElementById("entire-frame").style.opacity=1;
+}
