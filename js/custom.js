38cdaedba9f4cea31170234f7ddc6a779bc2f03a
TrushChauhan <tatva.pci203@outlook.com>
2025-01-31 18:27:34 +0530

    make

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,16 @@ function removeModal(){
     ele.style.display="none";
     document.getElementById("entire-frame").style.opacity=1;
 }
+const togglePassword = document
+            .querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+        togglePassword.addEventListener('click', () => {
+            // Toggle the type attribute using
+            // getAttribure() method
+            const type = password
+                .getAttribute('type') === 'password' ?
+                'text' : 'password';
+            password.setAttribute('type', type);
+            // Toggle the eye and bi-eye icon
+            this.classList.toggle('bi-eye');
+        });
