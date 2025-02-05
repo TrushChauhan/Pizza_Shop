52e955cc48370252de94df994af8ad6a254a25dc
TrushChauhan <tatva.pci203@outlook.com>
2025-02-05 18:22:19 +0530

    assignment-3,4

--- PizzaShopProject/js/custom.js
+++ PizzaShopProject/js/custom.js
@@ -7,16 +7,6 @@ function backToUsers(){
 function editUser(){
     window.location.href='edit-user.html'
 }
-function deleteUserModal(){
-    const ele=document.getElementById("modal-del");
-    ele.style.display="flex";
-    document.getElementById("entire-frame").style.opacity=0.7;
-}
-function removeModal(){
-    const ele=document.getElementById("modal-del");
-    ele.style.display="none";
-    document.getElementById("entire-frame").style.opacity=1;
-}
 const togglePassword = document
             .querySelector('#togglePassword');
         const password = document.querySelector('#password');
