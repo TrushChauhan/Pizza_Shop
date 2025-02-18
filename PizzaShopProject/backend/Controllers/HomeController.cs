bdb8ff932311adbb5f41a5149b09f6323cb566d9
TrushChauhan <tatva.pci203@outlook.com>
2025-02-18 18:51:48 +0530

    smtp mail

--- PizzaShopProject/backend/Controllers/HomeController.cs
+++ PizzaShopProject/backend/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 using System.IO.Pipelines;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace backend.Controllers;
 
@@ -10,10 +13,12 @@ public class HomeController : Controller
     private readonly ILogger<HomeController> _logger;
 
     private readonly PostgresContext _dbcontext;
-    public HomeController(ILogger<HomeController> logger,PostgresContext context)
+    private readonly IConfiguration _config;
+    public HomeController(ILogger<HomeController> logger,PostgresContext context,IConfiguration config)
     {
         _logger = logger;
-        this._dbcontext=context;
+        _dbcontext=context;
+        _config=config;
     }
 
     public IActionResult Index()
@@ -27,17 +32,17 @@ public class HomeController : Controller
         }
         return View();
     }
+   
     public IActionResult ForgotPassword()
     {
-        string userEmail=HttpContext.Session.GetString("Email");
-        var model=new UserDemo{Email=userEmail};
-        return View(model);
+        return View();
     }
 
     [HttpPost]
         public IActionResult Login(UserDemo model)
-        { 
-            var existingUser = _dbcontext.Userlogins.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+        {   string userPassword=encryptPassword(model.Password);
+            Console.WriteLine(userPassword);
+            var existingUser = _dbcontext.Userlogins.FirstOrDefault(u => u.Email == model.Email && u.Password == userPassword);
             if (existingUser != null)
             {
                 HttpContext.Session.SetString("Email",model.Email);
@@ -53,8 +58,54 @@ public class HomeController : Controller
             return RedirectToAction("Index","Home");
         }  
 
+        [HttpPost]
+     public IActionResult SendEmailLink(userEmail model){
+       string toemail=model.Email;
+        SendEmail(toemail);
+        return RedirectToAction("Index","Home");
+    }
+    public void SendEmail(string toemail){
+        try{
+            string emailBody = @"<html>
+                <body>
+
+                    <a href='http://localhost:5150/Home/ResetPassword'> Click Here</a>  
+                   </body>
+            </html>";
+            string smtpEmail = _config.GetValue<string>("SMTPCredentials:Email");
+            string smtpappPass = _config.GetValue<string>("SMTPCredentials:AppPass");
+
+            SmtpClient smtpclient = new SmtpClient("mail.etatvasoft.com"){
+                Port = 587,
+                Credentials = new NetworkCredential(smtpEmail,smtpappPass),
+                EnableSsl = true,
+            };
+
+            MailMessage mail = new MailMessage{
+                From= new MailAddress(smtpEmail),
+                Subject = "Reset Password for PizzaShop",
+                Body = emailBody,
+                IsBodyHtml = true,
+                
+            };
+            mail.IsBodyHtml=true;
+            mail.To.Add(toemail);
+            smtpclient.Send(mail);
+            return ;
+        }
+        catch(Exception ex){
+            throw(ex);  
+        }
+     }
+    public static string encryptPassword(string pass){
+        byte[] encode = new byte[pass.Length];
+        encode = System.Text.Encoding.UTF8.GetBytes(pass);
+        string encodedData = Convert.ToBase64String(encode);
+        return encodedData;
+    }
     public IActionResult ResetPassword()
-    {   
+    {
+        
         return View();
     }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ PizzaShopProject/backend/Models/userEmail.cs
@@ -0,0 +1,7 @@
+namespace backend.Models;
+
+public class userEmail
+{
+    public string Email { get; set; } = String.Empty;
+}
+
